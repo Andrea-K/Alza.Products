2d92d5ecf0bbcb69ff7846f5d52ad9e57077731c
Andrea Kozelkova <kozelkovaandrea94@gmail.com>
2026-01-12 23:11:50 +0100

    feat: switch from openapi to swagger + add v2

--- Alza.Products.WebApi/Program.cs
+++ Alza.Products.WebApi/Program.cs
@@ -1,23 +1,64 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.OpenApi;
+using Asp.Versioning;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+public partial class Program
 {
-    app.MapOpenApi();
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "Alza Products API",
+                Version = "v1"
+            });
+
+            options.SwaggerDoc("v2", new OpenApiInfo
+            {
+                Title = "Alza Products API",
+                Version = "v2"
+            });
+        });
+
+        builder.Services
+            .AddApiVersioning(options =>
+            {
+                options.DefaultApiVersion = new ApiVersion(1, 0);
+                options.AssumeDefaultVersionWhenUnspecified = true;
+                options.ReportApiVersions = true;
+            })
+            .AddApiExplorer(options =>
+            {
+                options.GroupNameFormat = "'v'VVV";
+                options.SubstituteApiVersionInUrl = true;
+            });
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("v1/swagger.json", "Alza Products API v1");
+                options.SwaggerEndpoint("v2/swagger.json", "Alza Products API v2");
+            });
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
