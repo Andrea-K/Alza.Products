d929afcaec89ff639f7aad49b3a01d303c0c6e67
Andrea Kozelkova <kozelkovaandrea94@gmail.com>
2026-01-12 22:35:51 +0100

    feat: add entities

---
+++ Alza.Products.Domain/Entities/EntityBase.cs
@@ -0,0 +1,14 @@
+﻿namespace Alza.Products.Domain.Entities
+{
+    public abstract class EntityBase
+    {
+        public Guid Id { get; init; } = Guid.NewGuid();
+        public DateTimeOffset DateCreated { get; set; } = DateTimeOffset.UtcNow;
+        public DateTimeOffset DateLastModified { get; set; } = DateTimeOffset.UtcNow;
+
+        public void UpdateLastModified()
+        {
+            this.DateLastModified = DateTimeOffset.UtcNow;
+        }
+    }
+}
---
+++ Alza.Products.Domain/Entities/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alza.Products.Domain.Entities
+{
+    public class Product : EntityBase
+    {
+        public string Name { get; set; }
+        public string ImgUri { get; set; }
+        public decimal Price { get; set; }
+        public string? Description { get; set; }
+    }
+}
