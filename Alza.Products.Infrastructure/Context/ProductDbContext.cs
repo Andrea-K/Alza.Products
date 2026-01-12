433ba4497038ce570a459835b5cb4f9682d2cba2
Andrea Kozelkova <kozelkovaandrea94@gmail.com>
2026-01-12 23:26:40 +0100

    feat(ef): add db context + factory

---
+++ Alza.Products.Infrastructure/Context/ProductDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Alza.Products.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Alza.Products.Infrastructure.Context
+{
+    public class ProductDbContext : DbContext
+    {
+        public ProductDbContext(DbContextOptions<ProductDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Product { get; set; }
+    }
+}
---
+++ Alza.Products.Infrastructure/Context/ProductDbContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Alza.Products.Infrastructure.Context
+{
+    public class ProductDbContextFactory : IDesignTimeDbContextFactory<ProductDbContext>
+    {
+        public ProductDbContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<ProductDbContext>();
+            optionsBuilder
+                .UseSqlServer(configuration["ConnectionStrings:DefaultConnection"]);
+
+            return new ProductDbContext(optionsBuilder.Options);
+        }
+    }
+}
